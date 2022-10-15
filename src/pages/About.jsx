f677d66f4225a0c2b890ba1d4a6888fcd344aa7b
PrakharRanjan2909 <rprakhar007@gmail.com>
2022-10-15 14:07:01 +0530

    user data getUser function in user jsx  error in map

---
+++ src/pages/About.jsx
@@ -0,0 +1,28 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details
+        <strong>
+          <a href="https://github.com/PrakharRanjan2909"> | Prakhar Ranjan</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Layout By:
+        <a
+          className="text-white"
+          href="https://www.facebook.com/settings?tab=account&section=username"
+        >
+          Pk
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default About;
